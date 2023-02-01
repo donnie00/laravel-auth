bfc36a1799bb05356a3f4cf099d1170112c76376
ippoRulez <ippolitodonato@gmail.com>
2023-02-01 15:52:07 +0100

    first commit and created migration seeder model and controller for the table

---
+++ database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $project = new Project();
+
+        $project->name = $faker->name();
+        $project->description = $faker->text();
+        $project->cover_img = $faker->imageUrl();
+        $project->github_link = $faker->url();
+
+        $project->save();
+    }
+}
